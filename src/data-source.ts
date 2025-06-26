5ea707ae81afa41d83b61055d5fc597f80a9c221
TehilaAizenbach <t0584161009@gmail.com>
2025-06-26 10:37:38 +0300

    cheange-datasource

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,24 +2,26 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import { Category } from "./categories/category.entity"; 
 import { ShoppingItem } from "./shoppingItems/shoppingItem.entity";
+import * as dotenv from "dotenv";
 
+dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "mssql",
-  host: "shoppinglist-sql-2406.database.windows.net",
-  port: 1433,
-  username: "sqladmin",
-  password: "tE110702",
-  database: "shoppinglist_db",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 1433,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   options: {
     encrypt: true,
     trustServerCertificate: false,
   },
-   extra: {
+  extra: {
     encrypt: true,
     trustServerCertificate: false,
   },
-  entities: [Category,ShoppingItem], 
+  entities: [Category, ShoppingItem],
   synchronize: true,
   logging: false,
 });
